565dba644eb73d7018db28f4b59b275a8b201101
MalikJaH <cr7.mjh@gmail.com>
2026-05-14 14:12:26 +0300

    Remove commented line in RecommendationChatController

--- src/main/java/com/no_mercy_no_doubt/tourism_booking/Ai/controller/RecommendationChatController.java
+++ src/main/java/com/no_mercy_no_doubt/tourism_booking/Ai/controller/RecommendationChatController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+//lao
 @RestController
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
